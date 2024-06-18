17c3b83bcb00ce86d49153479bab246400c1eac8
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-06-18 19:10:47 +0530

    deleted get random file from now on everything will be in one file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include "get_random.hpp"
+#include <string>
+#include <random>
 
 using namespace std;
 
-int main(){
-    const int handi_x = generateRandomNumber(4,7);
-    const int handi_y = generateRandomNumber(7,10);
+int generateRandomNumber(int lower_bound, int upper_bound) {
+    // Use a random device to seed the generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // Create a distribution in the range [lower_bound, upper_bound]
+    std::uniform_int_distribution<> distr(lower_bound, upper_bound);
+
+    // Generate and return a random number
+    return distr(gen);
+}
+
+int main()
+{
+    const int handi_x = generateRandomNumber(4, 7);
+    const int handi_y = generateRandomNumber(7, 10);
+
+    string rows[10] = {"_____","_____","_____","_____","_____","_____","_____","_____","_____",};
 
     return 0;
 }
