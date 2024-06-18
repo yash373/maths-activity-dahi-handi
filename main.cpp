b6ff3edd41a181647e7724fcb36940694a68f73c
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-06-18 18:36:15 +0530

    type changes and added row_box file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include "get_random.h"
+#include "get_random.hpp"
 
 using namespace std;
 
---
+++ row_box.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Box {
+    public:
+        Box(string type);
+};
