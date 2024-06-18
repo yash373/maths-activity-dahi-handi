3f03776925214ac9dc0bfa836a42ba417f4884ec
Yashodhan Mandal <85435491+yash373@users.noreply.github.com>
2024-06-18 18:26:09 +0530

    made hadi_x restricted so game is possible to be played

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main(){
-    const int handi_x = generateRandomNumber(1,10);
+    const int handi_x = generateRandomNumber(4,7);
     const int handi_y = generateRandomNumber(7,10);
 
     return 0;
